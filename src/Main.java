01ee0fd42da38a9c24dea9c44ab1520363b33b21
Aidan Swarthwood <amace10203@yahoo.com>
2023-09-14 12:51:11 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //intOperandA, intOperandB, intSum, intProduct, intDifference, intQuotient, intModulo
+        int intOperandA = 1;
+        int intOperandB = 6;
+    }
+}
